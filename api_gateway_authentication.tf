ca3fe1007b46634a4c6bb671035da5351def33c4
mcgigglepop <christoph.alano@gmail.com>
2021-01-05 02:20:40 -0600

    api gateway usage plan, key, key usage plan, key output

---
+++ api_gateway_authentication.tf
@@ -0,0 +1,26 @@
+# API Gateway Usage Plan
+resource "aws_api_gateway_usage_plan" "apigw_usage_plan" {
+  name = "${var.SERVICE}_usage_plan"
+
+  api_stages {
+    api_id = "${aws_api_gateway_rest_api.apigw_api_gateway.id}"
+    stage  = "${aws_api_gateway_deployment.deployment.stage_name}"
+  }
+}
+
+# API Gateway Key
+resource "aws_api_gateway_api_key" "apigw_auth_key" {
+  name = "${var.SERVICE}_auth_key"
+}
+
+# API Gateway Key Usage Plan
+resource "aws_api_gateway_usage_plan_key" "apigw_auth_key_usage_plan" {
+  key_id        = "${aws_api_gateway_api_key.apigw_auth_key.id}"
+  key_type      = "API_KEY"
+  usage_plan_id = "${aws_api_gateway_usage_plan.apigw_usage_plan.id}"
+}
+
+# Key Output
+output "apigw_auth_key" {
+    value = "${aws_api_gateway_api_key.apigw_auth_key.value}"
+}

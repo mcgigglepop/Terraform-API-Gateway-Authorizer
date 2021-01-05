49b62af7d2da50c1d6c0b79b0aca2c140f187dc1
mcgigglepop <christoph.alano@gmail.com>
2021-01-05 02:59:40 -0600

    added a dynamo db table that will be used for the authorizer function, as well the rest api

---
+++ dynamo_db.tf
@@ -0,0 +1,13 @@
+# Dynamo Database Resource
+resource "aws_dynamodb_table" "apigw_dynamodb_table" {
+  name           ="${var.STAGE}-apigw-table"
+  billing_mode   = "PROVISIONED"
+  read_capacity  = 1
+  write_capacity = 1
+  hash_key       = "uuid"
+
+  attribute {
+    name = "uuid"
+    type = "S"
+  }
+}

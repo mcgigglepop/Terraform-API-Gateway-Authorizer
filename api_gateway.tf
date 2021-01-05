1d9d437cf3f23ed498f2e0d881f9e0c0d3605344
mcgigglepop <christoph.alano@gmail.com>
2021-01-05 05:16:55 -0600

    variable cleanup

--- api_gateway.tf
+++ api_gateway.tf
@@ -1,9 +1,9 @@
 # API Gateway domain base path mapping
 resource "aws_api_gateway_base_path_mapping" "apigw_base_path" {
-  api_id      = "${aws_api_gateway_rest_api.apigw_api_gateway.id}"
-  domain_name = "${var.STAGE_DOMAIN_URL}"
-  stage_name  = "${var.SERVICE}"
-  base_path   = "${var.SERVICE}"
+  api_id      = aws_api_gateway_rest_api.apigw_api_gateway.id
+  domain_name = var.STAGE_DOMAIN_URL
+  stage_name  = var.SERVICE
+  base_path   = var.SERVICE
 }
 
 # Root API Gateway
@@ -13,36 +13,36 @@ resource "aws_api_gateway_rest_api" "apigw_api_gateway" {
 
 # API Gateway Deployment
 resource "aws_api_gateway_deployment" "deployment" {
-    rest_api_id   = "${aws_api_gateway_rest_api.apigw_api_gateway.id}"
-    stage_name    = "${var.SERVICE}"
+    rest_api_id   = aws_api_gateway_rest_api.apigw_api_gateway.id
+    stage_name    = var.SERVICE
     depends_on    = ["aws_api_gateway_integration.put_integration", "aws_api_gateway_integration.cors_integration_put"]
 }
 
 # Put Resource
 resource "aws_api_gateway_resource" "put_resource" {
   path_part   = "put"
-  parent_id   = "${aws_api_gateway_rest_api.apigw_api_gateway.root_resource_id}"
-  rest_api_id = "${aws_api_gateway_rest_api.apigw_api_gateway.id}"
+  parent_id   = aws_api_gateway_rest_api.apigw_api_gateway.root_resource_id
+  rest_api_id = aws_api_gateway_rest_api.apigw_api_gateway.id
 }
 
 # Put Method
 resource "aws_api_gateway_method" "put_method" {
-  rest_api_id       = "${aws_api_gateway_rest_api.apigw_api_gateway.id}"
-  resource_id       = "${aws_api_gateway_resource.put_resource.id}"
+  rest_api_id       = aws_api_gateway_rest_api.apigw_api_gateway.id
+  resource_id       = aws_api_gateway_resource.put_resource.id
   http_method       = "PUT"
   authorization     = "CUSTOM"
-  authorizer_id     = "${aws_api_gateway_authorizer.apigw_api_gateway_authorizer.id}"
+  authorizer_id     = aws_api_gateway_authorizer.apigw_api_gateway_authorizer.id
   api_key_required  = true
 }
 
 # Put Method Integration
 resource "aws_api_gateway_integration" "put_integration" {
-  rest_api_id             = "${aws_api_gateway_rest_api.apigw_api_gateway.id}"
-  resource_id             = "${aws_api_gateway_resource.put_resource.id}"
-  http_method             = "${aws_api_gateway_method.put_method.http_method}"
+  rest_api_id             = aws_api_gateway_rest_api.apigw_api_gateway.id
+  resource_id             = aws_api_gateway_resource.put_resource.id
+  http_method             = aws_api_gateway_method.put_method.http_method
   integration_http_method = "PUT"
   type                    = "AWS_PROXY"
-  uri                     = "${aws_lambda_function.put_lambda.invoke_arn}"
+  uri                     = aws_lambda_function.put_lambda.invoke_arn
   
   request_parameters = {
     "integration.request.header.X-Authorization" = "'static'"
@@ -61,28 +61,28 @@ EOF
 # Get Resource
 resource "aws_api_gateway_resource" "get_resource" {
   path_part   = "get"
-  parent_id   = "${aws_api_gateway_rest_api.apigw_api_gateway.root_resource_id}"
-  rest_api_id = "${aws_api_gateway_rest_api.apigw_api_gateway.id}"
+  parent_id   = aws_api_gateway_rest_api.apigw_api_gateway.root_resource_id
+  rest_api_id = aws_api_gateway_rest_api.apigw_api_gateway.id
 }
 
 # Get Method
 resource "aws_api_gateway_method" "get_method" {
-  rest_api_id       = "${aws_api_gateway_rest_api.apigw_api_gateway.id}"
-  resource_id       = "${aws_api_gateway_resource.get_resource.id}"
+  rest_api_id       = aws_api_gateway_rest_api.apigw_api_gateway.id
+  resource_id       = aws_api_gateway_resource.get_resource.id
   http_method       = "GET"
   authorization     = "CUSTOM"
-  authorizer_id     = "${aws_api_gateway_authorizer.apigw_api_gateway_authorizer.id}"
+  authorizer_id     = aws_api_gateway_authorizer.apigw_api_gateway_authorizer.id
   api_key_required  = true
 }
 
 # Get Method Integration
 resource "aws_api_gateway_integration" "get_integration" {
-  rest_api_id             = "${aws_api_gateway_rest_api.apigw_api_gateway.id}"
-  resource_id             = "${aws_api_gateway_resource.get_resource.id}"
-  http_method             = "${aws_api_gateway_method.get_method.http_method}"
+  rest_api_id             = aws_api_gateway_rest_api.apigw_api_gateway.id
+  resource_id             = aws_api_gateway_resource.get_resource.id
+  http_method             = aws_api_gateway_method.get_method.http_method
   integration_http_method = "GET"
   type                    = "AWS_PROXY"
-  uri                     = "${aws_lambda_function.get_lambda.invoke_arn}"
+  uri                     = aws_lambda_function.get_lambda.invoke_arn
   
   request_parameters = {
     "integration.request.header.X-Authorization" = "'static'"
@@ -101,28 +101,28 @@ EOF
 # Update Resource
 resource "aws_api_gateway_resource" "update_resource" {
   path_part   = "update"
-  parent_id   = "${aws_api_gateway_rest_api.apigw_api_gateway.root_resource_id}"
-  rest_api_id = "${aws_api_gateway_rest_api.apigw_api_gateway.id}"
+  parent_id   = aws_api_gateway_rest_api.apigw_api_gateway.root_resource_id
+  rest_api_id = aws_api_gateway_rest_api.apigw_api_gateway.id
 }
 
 # Update Method
 resource "aws_api_gateway_method" "update_method" {
-  rest_api_id       = "${aws_api_gateway_rest_api.apigw_api_gateway.id}"
-  resource_id       = "${aws_api_gateway_resource.update_resource.id}"
+  rest_api_id       = aws_api_gateway_rest_api.apigw_api_gateway.id
+  resource_id       = aws_api_gateway_resource.update_resource.id
   http_method       = "PATCH"
   authorization     = "CUSTOM"
-  authorizer_id     = "${aws_api_gateway_authorizer.apigw_api_gateway_authorizer.id}"
+  authorizer_id     = aws_api_gateway_authorizer.apigw_api_gateway_authorizer.id
   api_key_required  = true
 }
 
 # Update Method Integration
 resource "aws_api_gateway_integration" "update_integration" {
-  rest_api_id             = "${aws_api_gateway_rest_api.apigw_api_gateway.id}"
-  resource_id             = "${aws_api_gateway_resource.update_resource.id}"
-  http_method             = "${aws_api_gateway_method.update_method.http_method}"
+  rest_api_id             = aws_api_gateway_rest_api.apigw_api_gateway.id
+  resource_id             = aws_api_gateway_resource.update_resource.id
+  http_method             = aws_api_gateway_method.update_method.http_method
   integration_http_method = "PATCH"
   type                    = "AWS_PROXY"
-  uri                     = "${aws_lambda_function.get_lambda.invoke_arn}"
+  uri                     = aws_lambda_function.get_lambda.invoke_arn
   
   request_parameters = {
     "integration.request.header.X-Authorization" = "'static'"

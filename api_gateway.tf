e5cf112a46f797b3d9fa14e78e4a4477a7c8ef33
mcgigglepop <christoph.alano@gmail.com>
2021-01-05 04:38:38 -0600

    updated the method type for put, added get resource, method and method integration.

--- api_gateway.tf
+++ api_gateway.tf
@@ -29,7 +29,7 @@ resource "aws_api_gateway_resource" "put_resource" {
 resource "aws_api_gateway_method" "put_method" {
   rest_api_id   = "${aws_api_gateway_rest_api.apigw_api_gateway.id}"
   resource_id   = "${aws_api_gateway_resource.put_resource.id}"
-  http_method   = "POST"
+  http_method   = "PUT"
   authorization = "CUSTOM"
   authorizer_id = "${aws_api_gateway_authorizer.apigw_api_gateway_authorizer.id}"
   api_key_required = true
@@ -43,6 +43,46 @@ resource "aws_api_gateway_integration" "put_integration" {
   integration_http_method = "POST"
   type                    = "AWS_PROXY"
   uri                     = "${aws_lambda_function.put_lambda.invoke_arn}"
+  
+  request_parameters = {
+    "integration.request.header.X-Authorization" = "'static'"
+  }
+
+  # Transforms the incoming XML request to JSON
+  request_templates = {
+    "application/xml" = <<EOF
+{
+   "body" : $input.json('$')
+}
+EOF
+  }
+}
+
+# Get Resource
+resource "aws_api_gateway_resource" "get_resource" {
+  path_part   = "get"
+  parent_id   = "${aws_api_gateway_rest_api.apigw_api_gateway.root_resource_id}"
+  rest_api_id = "${aws_api_gateway_rest_api.apigw_api_gateway.id}"
+}
+
+# Get Method
+resource "aws_api_gateway_method" "get_method" {
+  rest_api_id   = "${aws_api_gateway_rest_api.apigw_api_gateway.id}"
+  resource_id   = "${aws_api_gateway_resource.get_resource.id}"
+  http_method   = "GET"
+  authorization = "CUSTOM"
+  authorizer_id = "${aws_api_gateway_authorizer.apigw_api_gateway_authorizer.id}"
+  api_key_required = true
+}
+
+# Get Method Integration
+resource "aws_api_gateway_integration" "get_integration" {
+  rest_api_id             = "${aws_api_gateway_rest_api.apigw_api_gateway.id}"
+  resource_id             = "${aws_api_gateway_resource.get_resource.id}"
+  http_method             = "${aws_api_gateway_method.get_method.http_method}"
+  integration_http_method = "POST"
+  type                    = "AWS_PROXY"
+  uri                     = "${aws_lambda_function.get_lambda.invoke_arn}"
   
   request_parameters = {
     "integration.request.header.X-Authorization" = "'static'"

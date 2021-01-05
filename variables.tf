fa500e4cb130260fdbabfe9dbe24b7cdfa164dd9
mcgigglepop <christoph.alano@gmail.com>
2021-01-05 05:03:38 -0600

    added variables for service and stage

--- variables.tf
+++ variables.tf
@@ -2,3 +2,9 @@ variable "AWS_REGION" {
     description = "Application deployment region"
     default     = "us-east-1"
 }
+variable "SERVICE" {
+    description = "Application service name"
+}
+variable "STAGE" {
+    description = "Application deployment stage"
+}

928d06944b112568d56c55da3ad6ad6f724a288a
mcgigglepop <christoph.alano@gmail.com>
2021-01-05 02:57:01 -0600

    added put resource, method, method integration

--- api_gateway.tf
+++ api_gateway.tf
@@ -17,3 +17,43 @@ resource "aws_api_gateway_deployment" "deployment" {
     stage_name    = "${var.SERVICE}"
     depends_on    = ["aws_api_gateway_integration.put_integration", "aws_api_gateway_integration.cors_integration_put"]
 }
+
+# Put Resource
+resource "aws_api_gateway_resource" "put_resource" {
+  path_part   = "put"
+  parent_id   = "${aws_api_gateway_rest_api.apigw_api_gateway.root_resource_id}"
+  rest_api_id = "${aws_api_gateway_rest_api.apigw_api_gateway.id}"
+}
+
+# Put Method
+resource "aws_api_gateway_method" "put_method" {
+  rest_api_id   = "${aws_api_gateway_rest_api.apigw_api_gateway.id}"
+  resource_id   = "${aws_api_gateway_resource.put_resource.id}"
+  http_method   = "POST"
+  authorization = "CUSTOM"
+  authorizer_id = "${aws_api_gateway_authorizer.apigw_api_gateway_authorizer.id}"
+  api_key_required = true
+}
+
+# Put Method Integration
+resource "aws_api_gateway_integration" "put_integration" {
+  rest_api_id             = "${aws_api_gateway_rest_api.apigw_api_gateway.id}"
+  resource_id             = "${aws_api_gateway_resource.put_resource.id}"
+  http_method             = "${aws_api_gateway_method.put_method.http_method}"
+  integration_http_method = "POST"
+  type                    = "AWS_PROXY"
+  uri                     = "${aws_lambda_function.put_lambda.invoke_arn}"
+  
+  request_parameters = {
+    "integration.request.header.X-Authorization" = "'static'"
+  }
+
+  # Transforms the incoming XML request to JSON
+  request_templates = {
+    "application/xml" = <<EOF
+{
+   "body" : $input.json('$')
+}
+EOF
+  }
+}

af3721a9d9d846816a342f1956523aed96e59d3b
mcgigglepop <christoph.alano@gmail.com>
2021-01-05 04:59:18 -0600

    resource reference cleanup

--- api_gateway_cors_options.tf
+++ api_gateway_cors_options.tf
@@ -1,16 +1,16 @@
 # CORS OPTIONS Method for Put Endpoint
 resource "aws_api_gateway_method" "cors_method_put" {
-  rest_api_id   = "${aws_api_gateway_rest_api.apigw_api_gateway.id}"
-  resource_id   = "${aws_api_gateway_resource.put_resource.id}"
+  rest_api_id   = aws_api_gateway_rest_api.apigw_api_gateway.id
+  resource_id   = aws_api_gateway_resource.put_resource.id
   http_method   = "OPTIONS"
   authorization = "NONE"
 }
 
 # CORS Integration for Put Endpoint
 resource "aws_api_gateway_integration" "cors_integration_put" {
-  rest_api_id = "${aws_api_gateway_rest_api.apigw_api_gateway.id}"
-  resource_id = "${aws_api_gateway_resource.put_resource.id}"
-  http_method = "${aws_api_gateway_method.cors_method_put.http_method}"
+  rest_api_id = aws_api_gateway_rest_api.apigw_api_gateway.id
+  resource_id = aws_api_gateway_resource.put_resource.id
+  http_method = aws_api_gateway_method.cors_method_put.http_method
   type                    = "MOCK"
   request_templates = {
     "application/json" = <<EOF
@@ -21,9 +21,9 @@ EOF
 
 # CORS Method Response for Put Endpoint
 resource "aws_api_gateway_method_response" "cors_method_response_put" {
-  rest_api_id = "${aws_api_gateway_rest_api.apigw_api_gateway.id}"
-  resource_id = "${aws_api_gateway_resource.put_resource.id}"
-  http_method = "${aws_api_gateway_method.cors_method_put.http_method}"
+  rest_api_id = aws_api_gateway_rest_api.apigw_api_gateway.id
+  resource_id = aws_api_gateway_resource.put_resource.id
+  http_method = aws_api_gateway_method.cors_method_put.http_method
 
   status_code = "200"
 
@@ -40,11 +40,11 @@ resource "aws_api_gateway_method_response" "cors_method_response_put" {
 
 # CORS Integration Response for Put Endpoint
 resource "aws_api_gateway_integration_response" "cors_integration_response_put" {
-  rest_api_id = "${aws_api_gateway_rest_api.apigw_api_gateway.id}"
-  resource_id = "${aws_api_gateway_method.cors_method_put.resource_id}"
-  http_method = "${aws_api_gateway_method.cors_method_put.http_method}"
+  rest_api_id = aws_api_gateway_rest_api.apigw_api_gateway.id
+  resource_id = aws_api_gateway_method.cors_method_put.resource_id
+  http_method = aws_api_gateway_method.cors_method_put.http_method
 
-  status_code = "${aws_api_gateway_method_response.cors_method_response_put.status_code}"
+  status_code = aws_api_gateway_method_response.cors_method_response_put.status_code
 
   response_parameters = {
     "method.response.header.Access-Control-Allow-Headers" = "'Content-Type,X-Amz-Date,Authorization,X-Api-Key,X-Amz-Security-Token'",
@@ -55,17 +55,17 @@ resource "aws_api_gateway_integration_response" "cors_integration_response_put" 
 
 # CORS OPTIONS Method for Get Endpoint
 resource "aws_api_gateway_method" "cors_method_get" {
-  rest_api_id   = "${aws_api_gateway_rest_api.apigw_api_gateway.id}"
-  resource_id   = "${aws_api_gateway_resource.get_resource.id}"
+  rest_api_id   = aws_api_gateway_rest_api.apigw_api_gateway.id
+  resource_id   = aws_api_gateway_resource.get_resource.id
   http_method   = "OPTIONS"
   authorization = "NONE"
 }
 
 # CORS Integration for Put Endpoint
 resource "aws_api_gateway_integration" "cors_integration_get" {
-  rest_api_id = "${aws_api_gateway_rest_api.apigw_api_gateway.id}"
-  resource_id = "${aws_api_gateway_resource.get_resource.id}"
-  http_method = "${aws_api_gateway_method.cors_method_get.http_method}"
+  rest_api_id = aws_api_gateway_rest_api.apigw_api_gateway.id
+  resource_id = aws_api_gateway_resource.get_resource.id
+  http_method = aws_api_gateway_method.cors_method_get.http_method
   type                    = "MOCK"
   request_templates = {
     "application/json" = <<EOF
@@ -76,9 +76,9 @@ EOF
 
 # CORS Method Response for Get Endpoint
 resource "aws_api_gateway_method_response" "cors_method_response_get" {
-  rest_api_id = "${aws_api_gateway_rest_api.apigw_api_gateway.id}"
-  resource_id = "${aws_api_gateway_resource.get_resource.id}"
-  http_method = "${aws_api_gateway_method.cors_method_get.http_method}"
+  rest_api_id = aws_api_gateway_rest_api.apigw_api_gateway.id
+  resource_id = aws_api_gateway_resource.get_resource.id
+  http_method = aws_api_gateway_method.cors_method_get.http_method
 
   status_code = "200"
 
@@ -95,11 +95,11 @@ resource "aws_api_gateway_method_response" "cors_method_response_get" {
 
 # CORS Integration Response for Get Endpoint
 resource "aws_api_gateway_integration_response" "cors_integration_response_get" {
-  rest_api_id = "${aws_api_gateway_rest_api.apigw_api_gateway.id}"
-  resource_id = "${aws_api_gateway_method.cors_method_get.resource_id}"
-  http_method = "${aws_api_gateway_method.cors_method_get.http_method}"
+  rest_api_id = aws_api_gateway_rest_api.apigw_api_gateway.id
+  resource_id = aws_api_gateway_method.cors_method_get.resource_id
+  http_method = aws_api_gateway_method.cors_method_get.http_method
 
-  status_code = "${aws_api_gateway_method_response.cors_method_response_get.status_code}"
+  status_code = aws_api_gateway_method_response.cors_method_response_get.status_code
 
   response_parameters = {
     "method.response.header.Access-Control-Allow-Headers" = "'Content-Type,X-Amz-Date,Authorization,X-Api-Key,X-Amz-Security-Token'",
@@ -110,17 +110,17 @@ resource "aws_api_gateway_integration_response" "cors_integration_response_get" 
 
 # CORS OPTIONS Method for Update Endpoint
 resource "aws_api_gateway_method" "cors_method_update" {
-  rest_api_id   = "${aws_api_gateway_rest_api.apigw_api_gateway.id}"
-  resource_id   = "${aws_api_gateway_resource.update_resource.id}"
+  rest_api_id   = aws_api_gateway_rest_api.apigw_api_gateway.id
+  resource_id   = aws_api_gateway_resource.update_resource.id
   http_method   = "OPTIONS"
   authorization = "NONE"
 }
 
 # CORS Integration for Update Endpoint
 resource "aws_api_gateway_integration" "cors_integration_update" {
-  rest_api_id = "${aws_api_gateway_rest_api.apigw_api_gateway.id}"
-  resource_id = "${aws_api_gateway_resource.update_resource.id}"
-  http_method = "${aws_api_gateway_method.cors_method_update.http_method}"
+  rest_api_id = aws_api_gateway_rest_api.apigw_api_gateway.id
+  resource_id = aws_api_gateway_resource.update_resource.id
+  http_method = aws_api_gateway_method.cors_method_update.http_method
   type                    = "MOCK"
   request_templates = {
     "application/json" = <<EOF
@@ -131,9 +131,9 @@ EOF
 
 # CORS Method Response for Update Endpoint
 resource "aws_api_gateway_method_response" "cors_method_response_update" {
-  rest_api_id = "${aws_api_gateway_rest_api.apigw_api_gateway.id}"
-  resource_id = "${aws_api_gateway_resource.update_resource.id}"
-  http_method = "${aws_api_gateway_method.cors_method_update.http_method}"
+  rest_api_id = aws_api_gateway_rest_api.apigw_api_gateway.id
+  resource_id = aws_api_gateway_resource.update_resource.id
+  http_method = aws_api_gateway_method.cors_method_update.http_method
 
   status_code = "200"
 
@@ -150,11 +150,11 @@ resource "aws_api_gateway_method_response" "cors_method_response_update" {
 
 # CORS Integration Response for update Endpoint
 resource "aws_api_gateway_integration_response" "cors_integration_response_update" {
-  rest_api_id = "${aws_api_gateway_rest_api.apigw_api_gateway.id}"
-  resource_id = "${aws_api_gateway_method.cors_method_update.resource_id}"
-  http_method = "${aws_api_gateway_method.cors_method_update.http_method}"
+  rest_api_id = aws_api_gateway_rest_api.apigw_api_gateway.id
+  resource_id = aws_api_gateway_method.cors_method_update.resource_id
+  http_method = aws_api_gateway_method.cors_method_update.http_method
 
-  status_code = "${aws_api_gateway_method_response.cors_method_response_update.status_code}"
+  status_code = aws_api_gateway_method_response.cors_method_response_update.status_code
 
   response_parameters = {
     "method.response.header.Access-Control-Allow-Headers" = "'Content-Type,X-Amz-Date,Authorization,X-Api-Key,X-Amz-Security-Token'",

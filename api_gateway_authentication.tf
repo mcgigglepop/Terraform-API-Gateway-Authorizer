5249098bfcc26800749ebb8b9c0d3956a1a9de79
mcgigglepop <christoph.alano@gmail.com>
2021-01-05 04:53:14 -0600

    resource reference cleanup

--- api_gateway_authentication.tf
+++ api_gateway_authentication.tf
@@ -3,8 +3,8 @@ resource "aws_api_gateway_usage_plan" "apigw_usage_plan" {
   name = "${var.SERVICE}_usage_plan"
 
   api_stages {
-    api_id = "${aws_api_gateway_rest_api.apigw_api_gateway.id}"
-    stage  = "${aws_api_gateway_deployment.deployment.stage_name}"
+    api_id = aws_api_gateway_rest_api.apigw_api_gateway.id
+    stage  = aws_api_gateway_deployment.deployment.stage_name
   }
 }
 
@@ -15,12 +15,12 @@ resource "aws_api_gateway_api_key" "apigw_auth_key" {
 
 # API Gateway Key Usage Plan
 resource "aws_api_gateway_usage_plan_key" "apigw_auth_key_usage_plan" {
-  key_id        = "${aws_api_gateway_api_key.apigw_auth_key.id}"
+  key_id        = aws_api_gateway_api_key.apigw_auth_key.id
   key_type      = "API_KEY"
-  usage_plan_id = "${aws_api_gateway_usage_plan.apigw_usage_plan.id}"
+  usage_plan_id = aws_api_gateway_usage_plan.apigw_usage_plan.id
 }
 
 # Key Output
 output "apigw_auth_key" {
-    value = "${aws_api_gateway_api_key.apigw_auth_key.value}"
+    value = aws_api_gateway_api_key.apigw_auth_key.value
 }

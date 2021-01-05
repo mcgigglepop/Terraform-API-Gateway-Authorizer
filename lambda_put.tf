a0302ebed6a1ceb3e7c25408e407dda73c3512ec
mcgigglepop <christoph.alano@gmail.com>
2021-01-05 04:16:02 -0600

    added resources for put endpoint, permission to allow execution from api gateway, cloudwatch log group, iam role, policy and attachment

---
+++ lambda_put.tf
@@ -0,0 +1,89 @@
+# Dynamo Put Function
+resource "aws_lambda_function" "put_lambda" {
+  filename          = "../.serverless/${var.SERVICE}.zip"
+  source_code_hash  = filebase64sha256("../.serverless/${var.SERVICE}.zip")
+  function_name     = "${var.SERVICE}-${var.STAGE}-put"
+  role          = aws_iam_role.put_lambda_role.arn
+  handler       = "functions/put.put"
+  runtime       = "nodejs12.x"
+  environment {
+    variables = {
+      DYNAMODB_TABLE = aws_dynamodb_table.apigw_dynamodb_table.id
+    }
+  }
+}
+
+# Permission to allow execution from api gateway to invoke the lambda function
+resource "aws_lambda_permission" "put_lambda_permission" {
+  statement_id  = "AllowExecutionFromAPIGatewayPut"
+  action        = "lambda:InvokeFunction"
+  function_name = "${aws_lambda_function.put_lambda.function_name}"
+  principal     = "apigateway.amazonaws.com"
+  source_arn = "arn:aws:execute-api:${var.AWS_REGION}:${data.aws_caller_identity.current.account_id}:${aws_api_gateway_rest_api.apigw_api_gateway.id}/*/${aws_api_gateway_method.put_method.http_method}${aws_api_gateway_resource.put_resource.path}"
+}
+
+# Cloudwatch Log Group Resource for the Put Function
+resource "aws_cloudwatch_log_group" "put_lambda_cw_group" {
+  name              = "/aws/lambda/${aws_lambda_function.put_lambda.function_name}"
+}
+
+# IAM Role for the Put Lambda Function
+resource "aws_iam_role" "put_lambda_role" {
+  name = "put_role"
+  assume_role_policy = <<POLICY
+{
+  "Version": "2012-10-17",
+  "Statement": [
+    {
+      "Action": "sts:AssumeRole",
+      "Principal": {
+        "Service": "lambda.amazonaws.com"
+      },
+      "Effect": "Allow",
+      "Sid": ""
+    }
+  ]
+}
+POLICY
+}
+
+
+# IAM Policy for the create/put log group/stream for the put function
+resource "aws_iam_policy" "put_lambda_logging_policy" {
+  name = "put_policy"
+  path = "/"
+  description = "IAM policy for logging from put lamdba"
+
+  policy = <<EOF
+{
+  "Version": "2012-10-17",
+  "Statement": [
+    {
+      "Action": [
+        "logs:CreateLogStream",
+        "logs:PutLogEvents"
+      ],
+      "Resource": "arn:aws:logs:*:*:*",
+      "Effect": "Allow",
+      "Sid": "VisualEditor0"
+    },
+    {
+            "Sid": "VisualEditor1",
+            "Effect": "Allow",
+            "Action": [
+                "dynamodb:PutItem"
+            ],
+            "Resource": [
+                "${aws_dynamodb_table.apigw_dynamodb_table.arn}"
+            ]
+        }
+  ]
+}
+EOF
+}
+
+# Policy attachment for put function role and policy
+resource "aws_iam_role_policy_attachment" "put_lambda_logs_policy_attachment" {
+  role = "${aws_iam_role.put_lambda_role.name}"
+  policy_arn = "${aws_iam_policy.put_lambda_logging_policy.arn}"
+}

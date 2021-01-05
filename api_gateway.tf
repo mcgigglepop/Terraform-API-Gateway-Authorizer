6e048435c40fe8baab1691cabf9609f0ceb3b9fb
mcgigglepop <christoph.alano@gmail.com>
2021-01-05 02:54:02 -0600

    added root api gateway resource, added api gateway deployment resource

--- api_gateway.tf
+++ api_gateway.tf
@@ -5,3 +5,15 @@ resource "aws_api_gateway_base_path_mapping" "apigw_base_path" {
   stage_name  = "${var.SERVICE}"
   base_path   = "${var.SERVICE}"
 }
+
+# Root API Gateway
+resource "aws_api_gateway_rest_api" "apigw_api_gateway" {
+  name = "${var.SERVICE}-${var.STAGE}-api-gateway"
+}
+
+# API Gateway Deployment
+resource "aws_api_gateway_deployment" "deployment" {
+    rest_api_id   = "${aws_api_gateway_rest_api.apigw_api_gateway.id}"
+    stage_name    = "${var.SERVICE}"
+    depends_on    = ["aws_api_gateway_integration.put_integration", "aws_api_gateway_integration.cors_integration_put"]
+}
